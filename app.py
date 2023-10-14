ab39fa8df01b410cb743928276d93ac7c66f93fa
peisomnoi <dorakuklina@gmail.com>
2023-10-14 16:01:23 +0200

    Logic "I recommend to" added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-gifrom flask import Flask, render_template, request
+from flask import Flask, render_template, request
 
 # Your code to calculate embeddings and find the best match
 from sentence_transformers import SentenceTransformer, util
@@ -16,23 +16,23 @@ def index():
 def search():
     model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     activity_descriptions = [
-        "Explore the historic Old Town of Linz with home of Johan Kepler.",
-        "Explore the heart of Austria's capital. Visit the Hofburg Palace, St. Stephen's Cathedral, and more.",
-        "Discover the magnificent Schönbrunn Palace, a UNESCO World Heritage site with beautiful gardens.",
-        "Enjoy a scenic cruise along the Danube River, taking in the city's stunning skyline.",
-        "Visit the Belvedere Palace and its world-renowned art collection.",
-        "Hike through the Vienna Woods, a beautiful natural area just outside the city.",
-        "Explore the historic Old Town of Salzburg, the birthplace of Mozart.",
-        "Visit the iconic fortress perched on a hill with panoramic views of Salzburg.",
-        "Follow in the footsteps of the Von Trapp family and visit film locations.",
-        "Stroll through the beautiful Mirabell Gardens, featured in the movie 'The Sound of Music.'",
-        "Take a tour of the salt mines in the nearby Dürrnberg region.",
-        "Discover the historic streets and landmarks of Linz, a vibrant Austrian city.",
-        "Explore the futuristic world of digital arts at the Ars Electronica Center.",
-        "Visit the historic Linz Castle and learn about its rich history.",
-        "Experience contemporary art at the Lentos Art Museum, located on the Danube River.",
-        "Take a ride on the Pöstlingbergbahn, a historic mountain tramway with panoramic views.",
-        "Hike through the Linz Woods, a beautiful natural area just outside the city."
+        "explore the historic Old Town of Linz with home of Johan Kepler.",
+        "explore the heart of Austria's capital. Visit the Hofburg Palace, St. Stephen's Cathedral, and more.",
+        "discover the magnificent Schönbrunn Palace, a UNESCO World Heritage site with beautiful gardens.",
+        "enjoy a scenic cruise along the Danube River, taking in the city's stunning skyline.",
+        "visit the Belvedere Palace and its world-renowned art collection.",
+        "hike through the Vienna Woods, a beautiful natural area just outside the city.",
+        "explore the historic Old Town of Salzburg, the birthplace of Mozart.",
+        "visit the iconic fortress perched on a hill with panoramic views of Salzburg.",
+        "follow in the footsteps of the Von Trapp family and visit film locations.",
+        "stroll through the beautiful Mirabell Gardens, featured in the movie 'The Sound of Music.'",
+        "take a tour of the salt mines in the nearby Dürrnberg region.",
+        "discover the historic streets and landmarks of Linz, a vibrant Austrian city.",
+        "explore the futuristic world of digital arts at the Ars Electronica Center.",
+        "visit the historic Linz Castle and learn about its rich history.",
+        "experience contemporary art at the Lentos Art Museum, located on the Danube River.",
+        "take a ride on the Pöstlingbergbahn, a historic mountain tramway with panoramic views.",
+        "hike through the Linz Woods, a beautiful natural area just outside the city."
     ]
     embeddings = model.encode(activity_descriptions)
     query = request.form['query']
